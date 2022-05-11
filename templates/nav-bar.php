64814d12ff39074589fbe5b9e44327e29e184edc
Farhan Khodaee <farhan@Farhans-MacBook-Pro.local>
2022-05-11 07:45:34 -0400

    https change1

--- templates/nav-bar.php
+++ templates/nav-bar.php
@@ -10,13 +10,13 @@
 
     // Require https
 
-    if( !($_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https') ){
-        if ($_SERVER['HTTPS'] != "on") {
-            $url = "https://". $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
-            header("Location: $url");
-            exit;
-        }
-    }
+    //if( !($_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https') ){
+        //if ($_SERVER['HTTPS'] != "on") {
+            //$url = "https://". $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
+           // header("Location: $url");
+           // exit;
+       // }
+    //}
 ?>
 
 <!DOCTYPE html>
